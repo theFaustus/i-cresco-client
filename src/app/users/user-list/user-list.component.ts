bcf7ad7733b46d49f790cb51af2a2fcebabc0611
ipascari <jhoonnyc@gmail.com>
2021-11-20 12:51:07 +0200

    feat: add filtering and pagination to the users table

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -6,6 +6,7 @@ import {NGXLogger} from 'ngx-logger';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import {UserService} from '../../core/services/user.service';
+import {MatPaginator} from '@angular/material';
 
 @Component({
     selector: 'app-user-list',
@@ -17,6 +18,7 @@ export class UserListComponent implements OnInit {
     dataSource;
 
     @ViewChild(MatSort, {static: true}) sort: MatSort;
+    @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
     constructor(
         private logger: NGXLogger,
@@ -24,6 +26,16 @@ export class UserListComponent implements OnInit {
         private userService: UserService,
         private titleService: Title
     ) {
+    }
+
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
     }
 
     ngOnInit() {
@@ -34,6 +46,7 @@ export class UserListComponent implements OnInit {
             .subscribe(users => {
                     this.dataSource = new MatTableDataSource(users);
                     this.dataSource.sort = this.sort;
+                    this.dataSource.paginator = this.paginator;
                 },
                 error => {
                     const resMessage = error.message || error.error.message || error.toString();
